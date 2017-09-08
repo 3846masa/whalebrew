94a625d808d9fb4d9f11ea996e70bc427026c926
3846masa <3846masahiro+git@gmail.com>
2017-09-08 10:26:49 +0900

    Fix windows installer

--- install.ps1
+++ install.ps1
@@ -1,23 +1,38 @@
-# Whalebrew for Windows
+# Whalebrew Installer for Windows
 
-$BINARY_URL = "https://github.com/bfirsh/whalebrew/releases/download/0.0.5/whalebrew-Windows-x86_64.exe"
-$USER_PATH = [Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
-$WHALEBREW_INSTALL_PATH = [Environment]::GetEnvironmentVariable("WHALEBREW_INSTALL_PATH", [System.EnvironmentVariableTarget]::User)
+try {
+  $orgErrorActionPreference = $ErrorActionPreference
+  $ErrorActionPreference = "Stop"
 
-if ($WHALEBREW_INSTALL_PATH -eq $null -or $WHALEBREW_INSTALL_PATH -eq '') {
-  $WHALEBREW_INSTALL_PATH = 'C:\whalebrew';
-  [Environment]::SetEnvironmentVariable("WHALEBREW_INSTALL_PATH", $WHALEBREW_INSTALL_PATH, [System.EnvironmentVariableTarget]::User)
+  $VERSION = "0.1.0"
+  $BINARY_URL = "https://github.com/3846masa/whalebrew/releases/download/$VERSION/whalebrew-Windows-x86_64.exe"
+  $USER_PATH = [Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
+  $WHALEBREW_INSTALL_PATH = [Environment]::GetEnvironmentVariable("WHALEBREW_INSTALL_PATH", [System.EnvironmentVariableTarget]::User)
+
+  if ($WHALEBREW_INSTALL_PATH -eq $null -or $WHALEBREW_INSTALL_PATH -eq '') {
+    $WHALEBREW_INSTALL_PATH = 'C:\whalebrew';
+    [Environment]::SetEnvironmentVariable("WHALEBREW_INSTALL_PATH", $WHALEBREW_INSTALL_PATH, [System.EnvironmentVariableTarget]::User)
+  }
+
+  if (![System.IO.Directory]::Exists($WHALEBREW_INSTALL_PATH)) {
+    [System.IO.Directory]::CreateDirectory($WHALEBREW_INSTALL_PATH)
+  }
+
+  if ($($USER_PATH).ToLower().Contains($($WHALEBREW_INSTALL_PATH).ToLower()) -eq $false) {
+    [Environment]::SetEnvironmentVariable("Path", "$USER_PATH;%WHALEBREW_INSTALL_PATH%", [System.EnvironmentVariableTarget]::User)
+  }
+
+  Write-Output "`nDownloading Whalebrew from : $BINARY_URL"
+
+  $WHALEBREW_PATH = Join-Path $WHALEBREW_INSTALL_PATH "whalebrew.exe"
+  (New-Object System.Net.WebClient).DownloadFile($BINARY_URL, "$WHALEBREW_PATH")
+
+  Write-Output "`nInstalled whalebrew to `"$WHALEBREW_PATH`"`n"
 }
-
-if (![System.IO.Directory]::Exists($WHALEBREW_INSTALL_PATH)) {
-  [System.IO.Directory]::CreateDirectory($WHALEBREW_INSTALL_PATH)
+catch {
+  $Host.UI.WriteErrorLine("`nFailed to install whalebrew...`n")
+  Write-Error $error[1]
 }
-
-if ($($USER_PATH).ToLower().Contains($($WHALEBREW_INSTALL_PATH).ToLower()) -eq $false) {
-  [Environment]::SetEnvironmentVariable("Path", "$USER_PATH;%WHALEBREW_INSTALL_PATH%", [System.EnvironmentVariableTarget]::User)
+finally {
+  $ErrorActionPreference = $orgErrorActionPreference
 }
-
-$WHALEBREW_PATH = Join-Path $WHALEBREW_INSTALL_PATH "whalebrew.exe"
-(New-Object System.Net.WebClient).DownloadFile($BINARY_URL, "$WHALEBREW_PATH")
-
-Write-Output "Installed whalebrew to `"$WHALEBREW_PATH`""
